e7ff188ac786cf890201f130a33b1806a06501a9
leechi22 <leechi2228@gmail.com>
2023-01-20 11:48:59 +0900

    #greeting
    로그인과 로그아웃을 같이 만들었다.

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form Input");
+const greeting = document.querySelector(".greeting");
+const logout = document.querySelector(".logout");
+
+const HIDDEN = "hidden"
+const USERNAME = "username"
+
+function loginHandler(event) {
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN);
+    localStorage.setItem(USERNAME, username);
+    logout.classList.remove(HIDDEN);
+    paintGreeting(username)
+}
+
+function logoutHandler() {
+    localStorage.removeItem(USERNAME);
+    logout.classList.add(HIDDEN);
+    greeting.classList.add(HIDDEN);
+    loginForm.classList.remove(HIDDEN);
+}
+
+logout.addEventListener("click", logoutHandler);
+
+function paintGreeting(username) {
+    greeting.classList.remove(HIDDEN);
+    greeting.innerText = `Hello ${username}`;
+}
+
+
+
+const savedUsername = localStorage.getItem(USERNAME);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN);
+    loginForm.addEventListener("submit", loginHandler);
+} else {
+    paintGreeting(savedUsername);
+
+}
